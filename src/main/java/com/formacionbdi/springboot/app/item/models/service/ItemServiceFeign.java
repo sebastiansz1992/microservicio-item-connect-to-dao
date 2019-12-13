85637ec9419d3c5a19fb6e54db4399df05121e73
sebastiansz1992 <sagudelo@kioniksolutions.com>
2019-12-12 23:22:00 -0500

    ms

---
+++ src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceFeign.java
@@ -0,0 +1,31 @@
+package com.formacionbdi.springboot.app.item.models.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import com.formacionbdi.springboot.app.item.clientes.IProductoClienteRest;
+import com.formacionbdi.springboot.app.item.models.Item;
+
+@Service("serviceFeign")
+@Primary
+public class ItemServiceFeign implements IItemService {
+
+	@Autowired
+	private IProductoClienteRest clienteFeign;
+	
+	@Override
+	public List<Item> findAll() {
+		return clienteFeign.listar().stream().map(c -> new Item(c, 1)).collect(Collectors.toList());
+	}
+
+	@Override
+	public Item findById(Long id, Integer cantidad) {
+		return new Item(clienteFeign.detalle(id), cantidad);
+	}
+
+	
+}
